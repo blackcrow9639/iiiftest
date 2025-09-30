e8b495db97fd5928fd94a09b7c92513e6cb3a454
blackcrow9639 <k_kurosaki@waseda.jp>
2025-09-30 13:51:25 +0900

    Add files via upload

---
+++ mk_iiifed.py
@@ -0,0 +1,173 @@
+#!/usr/bin/python3
+
+#各モジュールの読込み
+import sys
+import glob
+import os
+import re
+import csv
+import json
+#PILモジュールからImageを読込み
+from PIL import Image
+Image.MAX_IMAGE_PIXELS = 10000000000
+
+#画像処理ライブラリ（pyvips）のDLLパスを設定
+vipsbin = r'C:\work\waseda_IIIF\vips-dev-w64-web-8.17.2\vips-dev-8.17\bin'
+#add_dll_dir = getattr(os, 'add_dll_directory', None)
+os.environ['PATH'] = vipsbin + ';' + os.environ['PATH']
+
+# DLLパス確認用
+print("vipsbin:", vipsbin)
+print("PATH:", os.environ['PATH'])
+
+#if callable(add_dll_dir):
+#    add_dll_dir(vipsbin)
+#else:
+#    os.environ['PATH'] = os.pathsep.join((vipsbin, os.environ['PATH']))
+import pyvips
+
+
+#IIIFマニフェストや画像サーバのベースURLを定義
+base_url = 'https://iiif.archive.waseda.jp/iiif/manifest/ktnsk/'
+base_image_url = 'https://iiif.archive.waseda.jp/iiif/image/ktnsk/'
+all_bib = {}
+all_bib2 = {}
+bib_title = []
+
+#tiling_images関数で指定ディレクトリ内の画像ファイル（jpg, png, tif）をピラミッド型TIFFに変換
+#IIIF準拠の画像ディレクトリに保存
+def tiling_images(dir):
+    sdir = 'sources/'+dir
+    ifiles = glob.glob(sdir+'/*.jpg')
+    if len(ifiles) == 0:
+        ifiles = glob.glob(sdir+'/*.png')
+    if len(ifiles) == 0:
+        ifiles = glob.glob(sdir+'/*.tif')
+    tiled_file_dir = 'iiifed/'+dir
+    if not os.path.isdir(tiled_file_dir):
+        os.makedirs(tiled_file_dir)    
+    for ifile in ifiles:
+        image = pyvips.Image.new_from_file(ifile, access='sequential')
+        otfile = re.sub(r'sources/', 'iiifed/', os.path.splitext(ifile)[0])
+        tfile = otfile+'.tif'
+        if not os.path.isfile(otfile):
+            print (tfile)
+            image.tiffsave(tfile, tile=True, compression='jpeg', pyramid=True, tile_width=256, tile_height=256)
+            os.rename(tfile, otfile)
+            print (ifile+'を変換しました...')
+        else:
+            print (ifile+'はすでに存在します。')
+
+    print ('\r\nピラミッドTIFFへの画像変換が終了しました！')
+
+
+#メタデータCSVファイルを読み込み、各行を辞書形式で格納
+#mani_keys = ['dir','label','Description'] 
+mani_keys = ['dir','title','license','attribution','within','logo','viewingHint','viewingDirection']
+
+#各画像ディレクトリごとに画像ファイルを走査
+#IIIFマニフェストのmetadata・canvas・annotation情報を構築
+with open(sys.argv[1], newline='', encoding='utf_8_sig') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    rn = 0
+    for row in spamreader:
+        if rn == 0:
+            bib_title = row
+        else:
+            each_bib = {}
+            each_bib.update(zip(bib_title,row))
+            link_name = row[0]  
+            all_bib[link_name] = each_bib
+        rn = rn + 1;     
+#print (all_bib)
+
+#生成したマニフェスト情報をJSON形式で出力
+for key in all_bib.keys():
+    each_manifest = {}
+    all_meta = []
+    file_dir0 = key
+    tiling_images(key)
+    glob_name = 'iiifed/'+key+'/*'
+    #print (glob_name)
+    if os.path.isdir('iiifed/'+key):
+        list_file_names = glob.glob(glob_name)
+        for item in all_bib[key]:
+            if item not in mani_keys:
+                each_meta  = {} 
+                item_value = all_bib[key][item]
+                each_meta['label'] = item
+                each_meta['value'] = item_value
+                all_meta.append(each_meta)
+        each_manifest['@id'] = base_url+key+'/manifest.json'
+        each_manifest['@type'] = 'sc:Manifest'
+        #each_manifest['@context'] = 'http://iiif.io/api/presentation/2/context.json' 
+        each_manifest['@context'] = 'http://iiif.io/api/presentation/3/context.json' 
+        each_manifest['metadata'] = all_meta
+        each_manifest['attribution'] = "早稲田大学図書館 (Waseda University Library)"
+        each_manifest['license'] = "https://www.waseda.jp/library/user/using-images/"
+        for mani_key in mani_keys:
+            if all_bib[key].get(mani_key):
+                if mani_key == 'label':
+                    each_manifest['label'] = all_bib[key][mani_key]
+                elif mani_key == 'viewingDirection':
+                    each_manifest[mani_key] = all_bib[key][mani_key]
+                elif mani_key == 'Description':
+                    each_manifest['description'] = all_bib[key][mani_key]
+        cn = 0
+        sequence = {}
+        canvases = []
+        for file_path in list_file_names:
+            service = {}
+            resource = {}
+            mani_image = {}
+            canvas = {}
+            file_dir_path_list = os.path.split(file_path)[0]
+            file_dir = file_dir_path_list[0:len(file_dir_path_list)]
+            file_path = re.sub(r'\\', '/', file_path)
+            pr_file_path = re.sub(r'iiifed/', '', file_path)
+            if os.path.isdir(file_dir):
+              if not re.search('manifest.json', file_path):  
+                cn = cn + 1
+                canvas_number = 'p'+str(cn)+'.json'
+                image_url_id = base_image_url+pr_file_path
+                #20250916//presentation API 3 UPDATE
+                #service['@context'] = 'http://iiif.io/api/image/2/context.json' 
+                service['@context'] = 'http://iiif.io/api/image/3/context.json' 
+                service['@id']  = image_url_id
+                #Read the Image API Compliance document ("level[1-3]")
+                #20250916//presentation API 3 UPDATE
+                #service['profile'] = 'http://iiif.io/api/image/2/level1.json'
+                service['profile'] = 'http://iiif.io/api/image/3/level1.json'
+                img = Image.open(file_path)
+                width, height = img.size
+                resource['@type'] = 'dctypes:Image'
+                resource['format'] = 'image/jpeg'
+                resource['width'] = width
+                resource['height'] = height
+                resource['@id'] = image_url_id+'/full/full/0/default.jpg'
+                resource['service'] = service
+                mani_image['@type']  = 'oa:Annotation'
+                mani_image['motivation']  = 'sc:painting'
+                mani_image['resource']  = resource
+                mani_image['@id']  = base_url+file_dir0+'/annotation/'+canvas_number
+                mani_image['on']  = base_url+file_dir0+'/canvas/'+canvas_number
+                canvas['label'] = 'p. '+str(cn)
+                canvas['images'] = []
+                canvas['images'].append(mani_image)
+                canvas['width'] = width
+                canvas['height'] = height
+                canvas['@type'] = 'sc:Canvas'
+                canvas['@id'] = base_url+file_dir0+'/canvas/'+canvas_number
+                canvases.append(canvas)
+        sequence['@id'] =  base_url+file_dir0+'/sequence/s1.json'
+        sequence['@type'] =  'sc:Sequence'
+        sequence['label'] =  'Current Page Order'
+        sequence['canvases'] = canvases
+        each_manifest['sequences'] = []
+        each_manifest['sequences'].append(sequence)
+        write_file_path = 'iiifed/'+file_dir0+'/manifest.json'
+        print (write_file_path)
+        with open(write_file_path, mode='w',encoding='utf_8') as f:
+            json.dump(each_manifest, f, ensure_ascii=False,indent=2)
+        print ('manifest.jsonの生成が終わりました。')
+   
---
+++ mk_iiifed_Rev2.py
@@ -0,0 +1,166 @@
+#!/usr/bin/python3
+
+#各モジュールの読込み
+import sys
+import glob
+import os
+import re
+import csv
+import json
+from PIL import Image
+Image.MAX_IMAGE_PIXELS = 10000000000
+
+#画像処理ライブラリ[pyvips]のDLLパス設定してモジュールを読込
+#ローカルに[pivips]を展開し、そのbinディレクトリを指定
+vipsbin = r'C:\work\waseda_IIIF\vips-dev-w64-web-8.17.2\vips-dev-8.17\bin'
+os.environ['PATH'] = vipsbin + ';' + os.environ['PATH']
+import pyvips
+
+#IIIFマニフェストおよび画像サーバのベースURL定義
+base_url = 'https://iiif.archive.waseda.jp/iiif/manifest/ktnsk/'
+base_image_url = 'https://iiif.archive.waseda.jp/iiif/image/ktnsk/'
+#dict型[all_bib]={請求番号:{metadata項目:値, ...}, ...},[bib_title]=[metadata項目, ...]
+all_bib = {}
+bib_title = []
+
+#tiling_images関数で指定ディレクトリ内の画像ファイルをピラミッドTIFFに変換
+#予め[sources]ディレクトリを作成し、その配下に各画像セット用のサブディレクトリと末端に画像ファイルを配置
+def tiling_images(dir):
+    sdir = 'sources/' + dir
+    ifiles = glob.glob(sdir + '/*.jpg')
+    if len(ifiles) == 0:
+        ifiles = glob.glob(sdir + '/*.png')
+    if len(ifiles) == 0:
+        ifiles = glob.glob(sdir + '/*.tif')
+    tiled_file_dir = 'iiifed/' + dir
+    if not os.path.isdir(tiled_file_dir):
+        os.makedirs(tiled_file_dir)
+    for ifile in ifiles:
+        image = pyvips.Image.new_from_file(ifile, access='sequential')
+        otfile = re.sub(r'sources/', 'iiifed/', os.path.splitext(ifile)[0])
+        tfile = otfile + '.tif'
+        if not os.path.isfile(otfile):
+            print(tfile)
+            image.tiffsave(tfile, tile=True, compression='jpeg', pyramid=True, tile_width=256, tile_height=256)
+            os.rename(tfile, otfile)
+            print(ifile + 'を変換しました...')
+        else:
+            print(ifile + 'はすでに存在します。')
+    print('\r\nピラミッドTIFFへの画像変換が終了しました！')
+
+#マニフェストに追加するメタデータをCSVファイルから読み込む。各行をRDB形式で記載しておく
+mani_keys = ['dir', 'title', 'license', 'attribution', 'within', 'logo', 'viewingHint', 'viewingDirection']
+
+#各請求番号ディレクトリごとに画像ファイルを走査
+#metadataの項目と値を[each_bib]に格納し、[all_bib]に追加して、1レコードずつデータを格納
+with open(sys.argv[1], newline='', encoding='utf_8_sig') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    rn = 0
+    for row in spamreader:
+        if rn == 0:
+            bib_title = row
+        else:
+            each_bib = {}
+            each_bib.update(zip(bib_title, row))
+            link_name = row[0]
+            all_bib[link_name] = each_bib
+        rn += 1
+
+#IIIFマニフェスト生成処理
+for key in all_bib.keys():
+    manifest = {}
+    metadata = []
+    file_dir0 = key
+    tiling_images(key)
+    glob_name = 'iiifed/' + key + '/*'
+    if os.path.isdir('iiifed/' + key):
+        list_file_names = glob.glob(glob_name)
+        # メタデータ追加処理
+        for item in all_bib[key]:
+            if item not in mani_keys:
+                item_value = all_bib[key][item]
+                metadata.append({
+                    "label": {"ja": [item]},
+                    "value": {"ja": [item_value]}
+                })
+            manifest['@context'] = 'http://iiif.io/api/presentation/3/context.json'
+            manifest['id'] = base_url + key + '/manifest.json'
+            manifest['type'] = 'Manifest'
+            manifest['label'] = {"ja": [all_bib[key].get('title', key)]}
+            manifest['metadata'] = metadata
+            manifest['attribution'] = {"ja": ["早稲田大学図書館 (Waseda University Library)"]}
+            manifest['license'] = "https://www.waseda.jp/library/user/using-images/"
+            
+            #以下は条件付きでメタデータを追加
+            #ロゴイメージ
+                manifest['logo'] = [{
+                    "id": all_bib[key]['logo'],
+                    "type": "Image"
+                }]
+            #"viewingDirection" [left-to-right](the default if the property is not specified),[right-to-left],[top-to-bottom],[bottom-to-top]
+            if all_bib[key].get('viewingDirection'):
+                manifest['viewingDirection'] = all_bib[key]['viewingDirection']
+            # Canvas配列
+            items = []
+            cn = 0
+            for file_path in list_file_names:
+                file_path = re.sub(r'\\', '/', file_path)
+                pr_file_path = re.sub(r'iiifed/', '', file_path)
+            #画像ファイルをCanvasとしてマニフェストに追加処理
+            if not re.search('manifest.json', file_path):
+                cn += 1
+                canvas_number = 'p' + str(cn) + '.json'
+                image_url_id = base_image_url + pr_file_path
+                img = Image.open(file_path)
+                width, height = img.size
+                #Canvas情報の作成
+                canvas_id = base_url + file_dir0 + '/canvas/' + canvas_number
+                canvas = {
+                    "id": canvas_id,
+                    "type": "Canvas",
+                    "label": {"ja": [f"p. {cn}"]},
+                    "width": width,
+                    "height": height,
+                    "items": []
+                }
+                #AnnotationPage情報の作成
+                annotation_page_id = canvas_id + '/annotationpage'
+                annotation_page = {
+                    "id": annotation_page_id,
+                    "type": "AnnotationPage",
+                    "items": []
+                }
+                #Annotation情報の作成
+                annotation_id = canvas_id + '/annotation'
+                annotation = {
+                    "id": annotation_id,
+                    "type": "Annotation",
+                    "motivation": "painting",
+                    "body": {
+                        "id": image_url_id + '/full/full/0/default.jpg',
+                        "type": "Image",
+                        "format": "image/jpeg",
+                        "width": width,
+                        "height": height,
+                        "service": [{
+                            "id": image_url_id,
+                            "type": "ImageService3",
+                            "profile": "level1",
+                            "width": width,
+                            "height": height
+                        }]
+                    },
+                    "target": canvas_id
+                }
+                #生成したannotationをannotation_pageに、annotation_pageをcanvasに追加
+                annotation_page["items"].append(annotation)
+                canvas["items"].append(annotation_page)
+                items.append(canvas)
+        #manifestにCanvas配列を追加して、JSONファイルとして保存
+        manifest["items"] = items
+        #書き出しファイルパスの作成とJSONファイルの保存
+        write_file_path = 'iiifed/' + file_dir0 + '/manifest.json'
+        print(write_file_path) 
+        with open(write_file_path, mode='w', encoding='utf_8') as f:
+            json.dump(manifest, f, ensure_ascii=False, indent=2)
+        print('manifest.jsonの生成が終わりました。')
